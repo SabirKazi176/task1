e929dd40ffc6446b0664da5297fb39b11e0b792e
SabirKazi176 <sabirkazi176@gmail.com>
2024-10-22 23:41:14 +0530

    doubled

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function clicked() {
   let output = document.getElementsByTagName("input")[0].value;
   document.getElementsByTagName("h1")[0].innerHTML = output;
 }
+function clickeded() {
+  let output = document.getElementsByTagName("input")[1].value;
+  document.getElementsByTagName("h1")[1].innerHTML = output;
+}
